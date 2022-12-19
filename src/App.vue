2ca039f00d75976c4115b18570e012d69435aa08
Flora Preuvot <flora@qomon.com>
2022-12-19 19:58:05 +0100

    add sorted by size feature

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,33 @@
       <div>
         <img src="/assets/tree.svg" alt="Christmas tree" />
       </div>
-      <div class="mt-2 flex justify-center items-center">
+
+      <div class="mt-2 mb-8 flex justify-center items-center">
         <img
-          v-for="present in presents"
+          v-for="present in presentsSorted"
           :key="present.id"
           :src="present.src"
           :alt="`Present ${present.id}`"
           data-qa="present"
         />
       </div>
+
+      <div class="w-full flex justify-center">
+        <button class="bg-black text-white font-semibold rounded-lg px-6 py-3" @click="toggleSort">Toggle sort</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import presents from './presents.json'
+
+let isSortedBySize = ref(false)
+
+const sortBySize = (a, b) => a.dimensions.width * a.dimensions.height - b.dimensions.width * b.dimensions.height
+
+const presentsSorted = computed(() => (isSortedBySize.value ? [...presents].sort(sortBySize) : presents))
+
+const toggleSort = () => (isSortedBySize.value = !isSortedBySize.value)
 </script>
